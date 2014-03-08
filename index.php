89f29b22048ec3ae343cd121aa4b5f0e7e7ffe1d
miloha87 <ekira73@gmail.com>
2014-03-08 00:18:27 -0500

    creacion del proyecto basico

---
+++ index.php
@@ -0,0 +1,28 @@
+<!DOCTYPE html>
+<!--
+To change this license header, choose License Headers in Project Properties.
+To change this template file, choose Tools | Templates
+and open the template in the editor.
+-->
+<html>
+    <head>
+        <meta charset="UTF-8">
+        <title></title>
+        <link rel="stylesheet" media="all" href="web/css/bootstrap.min.css" />
+        <link rel="stylesheet" media="all" href="web/css/bootstrap-theme.min.css" />
+    </head>
+    <body>
+        <div>
+            <div>
+                <div><span>Key: </span><input type="text" value="Search"/></div>
+                <div></div>
+            </div>
+            <div>
+                <div></div>
+            </div>
+        </div>
+        <script type="text/javascript" src="web/js/jquery-1.11.0.min.js"></script>
+        <script type="text/javascript" src="web/js/bootstrap.min.js"></script>
+        <script type="text/javascript" src="web/js/main.js"></script>
+    </body>
+</html>
