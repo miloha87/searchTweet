09cc3792dc408bb4b2d78e50982ac2a927e3d5a2
miloha87 <ekira73@gmail.com>
2014-03-08 02:27:07 -0500

    Creación css y modificación estructura

--- index.php
+++ index.php
@@ -10,15 +10,21 @@ and open the template in the editor.
         <title></title>
         <link rel="stylesheet" media="all" href="web/css/bootstrap.min.css" />
         <link rel="stylesheet" media="all" href="web/css/bootstrap-theme.min.css" />
+        <link rel="stylesheet" media="all" href="web/css/main.css" />
+        <link rel="stylesheet" media="all" href="web/js/main.js" />
     </head>
     <body>
-        <div>
-            <div>
-                <div><span>Key: </span><input type="text" value="Search"/></div>
-                <div></div>
+        <div class="content-main">
+            <div id="logo"></div>
+            <div id="header-search">
+                <span>Ingrese la palabra por la que desee filtrar los Tweets</span>
+                <form action="" class="navbar-search pull-left button1" >
+                    <input  type="text" class="search-query" placeholder="Search">
+                </form>
             </div>
-            <div>
-                <div></div>
+            <div class="content">
+                <div id="content-left"></div>
+                <div id="content-right"></div>
             </div>
         </div>
         <script type="text/javascript" src="web/js/jquery-1.11.0.min.js"></script>
