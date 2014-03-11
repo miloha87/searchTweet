bc128765631fc6eec1bdaebb769f612090a3a3d5
miloha87 <ekira73@gmail.com>
2014-03-11 16:25:31 -0500

    Update index.php

--- index.php
+++ index.php
@@ -1,38 +1,35 @@
-<?php if( !empty($_SERVER['HTTP_X_REQUESTED_WITH']) && strtolower($_SERVER['HTTP_X_REQUESTED_WITH']) == 'xmlhttprequest'): ?>
-    <?php include 'search.php'; ?>
-<?php else: ?>
-    <!DOCTYPE html>
-    <!--
-    To change this license header, choose License Headers in Project Properties.
-    To change this template file, choose Tools | Templates
-    and open the template in the editor.
-    -->
-    <html>
-        <head>
-            <meta charset="UTF-8">
-            <title></title>
-            <link rel="stylesheet" media="all" href="web/css/bootstrap.min.css" />
-            <link rel="stylesheet" media="all" href="web/css/bootstrap-theme.min.css" />
-            <link rel="stylesheet" media="all" href="web/css/main.css" />
-        </head>
-        <body>
-            <div class="content-main">
-                <div id="logo"></div>
+<!DOCTYPE html>
+<!--
+To change this license header, choose License Headers in Project Properties.
+To change this template file, choose Tools | Templates
+and open the template in the editor.
+-->
+<html>
+    <head>
+        <meta charset="UTF-8">
+        <title></title>
+        <link rel="stylesheet" media="all" href="web/css/bootstrap.min.css" />
+        <link rel="stylesheet" media="all" href="web/css/bootstrap-theme.min.css" />
+        <link rel="stylesheet" media="all" href="web/css/main.css" />
+    </head>
+    <body>
+        <div class="content-main">
+            <div id="logo"></div>
+            <div id="header-search">
+                <span>Ingrese la palabra por la que desee filtrar los Tweets</span>
                 <div id="header-search">
-                    <span>Ingrese la palabra por la que desee filtrar los Tweets</span>
-                    <div id="header-search">
-                        <form  method="GET" action="web/php/insert.php">
-                            <input id="data" name="query" type="text" placeholder="Ingresa palabra" />
-                            <input id="search" type="submit" value="Buscar" />
-                        </form>
-                     </div>
+                    <form  method="GET" action="search.php">
+                        <input id="data" name="query" type="text" placeholder="Ingresa palabra" />
+                        <input id="search" type="submit" value="Buscar" />
+                    </form>
+                 </div>
+            </div>
+            <div class="content">
+                <div id="tweets">
+                     Tweets will go here.
                 </div>
-                <div class="content">
-                    <div id="tweets">
-                         Tweets will go here.
-                    </div>
-                       <div id="content-right">
-                       <?php
+                <div id="content-right">
+                    <?php
                             include("web/php/connect.php");
 
                             $result = mysql_query("SELECT distinct name FROM searchs"); 
@@ -42,13 +39,11 @@
                             } 
 
                         ?>
-
-                    </div>
                 </div>
             </div>
-            <script type="text/javascript" src="web/js/jquery-1.11.0.min.js"></script>
-            <script type="text/javascript" src="web/js/bootstrap.min.js"></script>
-            <script type="text/javascript" src="web/js/main.js"></script>
-        </body>
-    </html>
-<?php endif; ?>
+        </div>
+        <script type="text/javascript" src="web/js/jquery-1.11.0.min.js"></script>
+        <script type="text/javascript" src="web/js/bootstrap.min.js"></script>
+        <script type="text/javascript" src="web/js/main.js"></script>
+    </body>
+</html>
