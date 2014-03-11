11d543d4b4ad8a8e5feac4e09ae8318fa0c9dc06
miloha87 <ekira73@gmail.com>
2014-03-11 16:35:15 -0500

    Update insert.php

--- web/php/insert.php
+++ web/php/insert.php
@@ -1,11 +1,7 @@
 <?php
+    include("connect.php");
+    $data = $_GET["query"];
 
-	include("connect.php");
-	
-	$data = $_GET["data"];
-	
-	//$gravarsql= "insert into searchs (name) value ('"$data"')";
-	mysql_query("insert into searchs (name) value ('".$data."')");
-	header('Location: ../../index.php');
+    mysql_query("insert into searchs (name) value ('".$data."')");
 ?>
 
